5eb25a9ed7a639168e79c5c24162c1ecfd1ba5ec
KoudaiChiba <6bda2209@mail.u-tokai.ac.jp>
2019-01-20 14:02:09 +0900

    PinBallプロジェクト　初コミット
    課題の内容に調整しました。

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+
+    //ボールが見える可能性のあるｚ軸の最大値
+    private float visiblePosZ = -6.5f;
+
+    //ゲームオーバを表示するテキスト
+    private GameObject gameoverText;
+
+    //スコアを表示するテキスト
+    private GameObject scoreText;
+
+    //得点
+    private int score = 0;
+
+	// Use this for initialization
+	void Start () {
+        //シ―ンの中のGameOverTextオブジェクトを取得
+        this.gameoverText = GameObject.Find("GameOverText");
+
+        //シーン中のscoreTextオブジェクトを取得
+        this.scoreText = GameObject.Find("ScoreText");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//ボールが画面外に出て場合
+        if(this.transform.position.z < this.visiblePosZ)
+        {
+            //GameOverTextにゲームオーバを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+       if(other.gameObject.tag == "SmallStarTag" || other.gameObject.tag == "SmallCloudTag")
+        {
+            this.score += 10;
+
+            this.scoreText.GetComponent<Text>().text = "Score" + this.score;
+        }
+
+       if(other.gameObject.tag == "LargeStarTag" || other.gameObject.tag == "LargeCloudTag")
+        {
+            this.score += 20;
+
+            this.scoreText.GetComponent<Text>().text = "Score" + this.score;
+        }
+    }
+}
---
+++ Assets/Scripts/FriperController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FriperController : MonoBehaviour {
+    //HingeJointコンポーネントを入れる
+    private HingeJoint myHingeJoint;
+
+    //初期の傾き
+    private float defaultAngle = 20;
+    //弾いた時の傾き
+    private float flickAngle = -20;
+
+    private bool isLButtonDown = false;
+
+    private bool isRButtonDown = false;
+
+    // Use this for initialization
+	void Start () {
+        //HingeJointコンポーネント取得
+        this.myHingeJoint = GetComponent<HingeJoint>();
+
+        //フリッパーの傾きを設定
+        SetAngle(this.defaultAngle);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //右矢印キーを押したとき左フリッパーを動かす
+        if (this.isLButtonDown == true && tag == "LeftFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+        //右矢印キーを押したとき右フリッパーを動かす
+        if(this.isRButtonDown == true && tag == "RightFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+
+        //矢印キーが離されたときフリッパーをもとに戻す
+        if(this.isLButtonDown == false && tag == "LeftFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+        if(this.isRButtonDown == false && tag == "RightFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+
+    }
+
+    //フリッパーの傾きを設定
+    public void SetAngle(float angle)
+    {
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+
+    //左ボタンを押し続けた場合の処理（追加）
+    public void GetMyLeftButtonDown()
+    {
+        this.isLButtonDown = true;
+    }
+    //左ボタンを離した場合の処理（追加）
+    public void GetMyLeftButtonUp()
+    {
+        this.isLButtonDown = false;
+    }
+
+    //右ボタンを押し続けた場合の処理（追加）
+    public void GetMyRightButtonDown()
+    {
+        this.isRButtonDown = true;
+    }
+    //右ボタンを離した場合の処理（追加）
+    public void GetMyRightButtonUp()
+    {
+        this.isRButtonDown = false;
+    }
+}
